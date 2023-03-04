edead1326db19e87daa2f47da417588ff55e66fe
Guru <gkudtecoding@gmail.com>
2023-03-04 23:56:20 +0530

    Added Header

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
-const express = require("express");
-const colors = require('colors');
+// import { ConnectDB } from './src/utils/connectDB';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import colors from 'colors';
+import ConnectDB from './src/utils/connect.js';
+dotenv.config();
+ConnectDB();
 const app = express();
+
+// console.log(process.env.PORT)
 
 app.listen(5000, () => {
     console.log(colors.yellow.underline(`Server Succesfully running on http://localhost:5000`));
 });
+
+
---
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import './landingpage.css';
+
+const LandingPage = () => {
+  return (
+    <div className='landing-page'>
+      <Header/>
+    </div>
+  )
+}
+
+
+export default LandingPage;
