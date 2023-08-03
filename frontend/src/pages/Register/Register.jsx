c332b9c0e36abd3e3c4de5ea21bfd40de19e4efd
Pratik Patil <pratik8560@gmail.com>
2023-08-03 22:51:16 +0530

    updated filters option in requests and registration form validation in frontend

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,51 +1,53 @@
-
-import React, { useState, useContext } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
-import axios from 'axios';
+import axios from "axios";
 import maleImage from "../../assets/male.png";
 import femaleImage from "../../assets/female.png";
 import "./Register.css";
 
 const Register = () => {
   const [user, setUser] = useState({
-    firstname: '',
-    lastname: '',
-    village: '',
-    city: '',
-    district: '',
-    state: '',
-    pincode: '',
-    email: '',
-    phone: '',
-    password: '',
+    firstname: "",
+    lastname: "",
+    village: "",
+    city: "",
+    district: "",
+    state: "",
+    pincode: "",
+    email: "",
+    phone: "",
+    password: "",
   });
 
-  const { isLoggedIn, setIsLoggedIn, setFirstName} = useContext(AuthContext);
+  const [selectedGender, setSelectedGender] = useState("");
+  const { isLoggedIn, setIsLoggedIn, setFirstName } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleRegistration = async (e) => {
     e.preventDefault();
+    if (!selectedGender) {
+      window.alert("Please select your gender.");
+      return;
+    }
     try {
-
-      const response = await axios.post(process.env.REACT_APP_BACKEND_URL+'/api/user/register', user); 
+      const response = await axios.post(process.env.REACT_APP_BACKEND_URL + "/api/user/register", user);
       if (response.status === 200) {
         setFirstName(response.data.firstName);
         setIsLoggedIn(true);
         window.alert("Registration successful");
-        navigate('/dashboard'); 
+        navigate("/dashboard");
       }
     } catch (error) {
       console.log(error);
-      if(error.response && error.response.status===400){
-        window.alert("User already exist");
-      }
-      else{
+      if (error.response && error.response.status === 400) {
+        window.alert("User already exists");
+      } else {
         window.alert("There was an error");
       }
-      
     }
   };
+
 
   return (
     <>
@@ -68,6 +70,7 @@ const Register = () => {
                 placeholder="Type your First name"
                 value={user.firstname}
                 onChange={(e) => setUser({ ...user, firstname: e.target.value })}
+                required
               />
             </div>
             <div className="col">
@@ -81,30 +84,41 @@ const Register = () => {
                 placeholder="Type your Last name"
                 value={user.lastname}
                 onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+                required
               />
             </div>
           </div>
 
           <div className="row">
-            <div className="col">
-              <label className="register-lable">Gender</label>
-              <div className="gender-buttons">
-                <button type="button" className="transparent-blue" 
-                  onClick={(e) => setUser({ ...user, gender: "male" })}>
-                  <img src={maleImage} alt="Male" className="gender-icon" />
-                  Male
-                </button>
-                <button type="button" className="transparent-blue" 
-                  onClick={(e) => setUser({ ...user, gender: "female" })}>
-                  <img src={femaleImage} alt="Female" className="gender-icon" />
-                  Female
-                </button>
-                <button type="button" className="transparent-blue"
-                  onClick={(e) => setUser({ ...user, gender: "other" })}>
-                  Other
-                </button>
+          <div className="col">
+                <label className="register-lable">Gender</label>
+                <div className="gender-buttons">
+                  <button
+                    type="button"
+                    className={selectedGender === "male" ? "blue" : "transparent-blue"}
+                    onClick={() => setSelectedGender("male")}
+                  >
+                    <img src={maleImage} alt="Male" className="gender-icon" />
+                    Male
+                  </button>
+                  <button
+                    type="button"
+                    className={selectedGender === "female" ? "blue" : "transparent-blue"}
+                    onClick={() => setSelectedGender("female")}
+                  >
+                    <img src={femaleImage} alt="Female" className="gender-icon" />
+                    Female
+                  </button>
+                  <button
+                    type="button"
+                    className={selectedGender === "other" ? "blue" : "transparent-blue"}
+                    onClick={() => setSelectedGender("other")}
+                  >
+                    Other
+                  </button>
+                </div>
               </div>
-            </div>
+
             <div className="col">
               <label htmlFor="mobile" className="register-lable">
                 Mobile Number
@@ -113,9 +127,12 @@ const Register = () => {
                 type="tel"
                 id="mobile"
                 name="mobile"
-                placeholder="+91 9598929100"
+                placeholder="9598929100"
+                pattern="[0-9]{10}"
+                title="Enter 10 digit number"
                 value={user.phone}
                 onChange={(e) => setUser({ ...user, phone: e.target.value })}
+                required
               />
             </div>
           </div>
@@ -132,6 +149,7 @@ const Register = () => {
                 placeholder="example@email.com"
                 value={user.email}
                 onChange={(e) => setUser({ ...user, email: e.target.value })}
+                required
               />
             </div>
           </div>
@@ -148,6 +166,7 @@ const Register = () => {
                 placeholder="Type your village"
                 value={user.village}
                 onChange={(e) => setUser({ ...user, village: e.target.value })}
+                required
               />
             </div>
             <div className="col">
@@ -161,6 +180,7 @@ const Register = () => {
                 placeholder="Type your district"
                 value={user.district}
                 onChange={(e) => setUser({ ...user, district: e.target.value })}
+                required
               />
             </div>
           </div>
@@ -176,6 +196,7 @@ const Register = () => {
                 placeholder="Type Your city"
                 value={user.city}
                 onChange={(e) => setUser({ ...user, city: e.target.value })}
+                required
               />
             </div>
             <div className="col">
@@ -189,6 +210,7 @@ const Register = () => {
                 placeholder="Type your State"
                 value={user.state}
                 onChange={(e) => setUser({ ...user, state: e.target.value })}
+                required
               />
             </div>
           </div>
@@ -204,6 +226,7 @@ const Register = () => {
                 placeholder="Type your pincode"
                 value={user.pincode}
                 onChange={(e) => setUser({ ...user, pincode: e.target.value })}
+                required
               />
             </div>
             <div className="col">
@@ -229,9 +252,6 @@ const Register = () => {
           Already registered? click here.
         </div>
         </form>
-
-        
-
       </div>
     </div>
     </>
