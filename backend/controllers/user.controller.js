598646a0235a6fcfe1386b4ea3df10da0d9fd8fe
Pratik Patil <pratik8560@gmail.com>
2024-02-21 22:13:07 +0530

    test

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -182,7 +182,6 @@ const userLogout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      sameSite: "none",
     });
 
     return res.status(200).json({ message: "Logged out successfully" });
